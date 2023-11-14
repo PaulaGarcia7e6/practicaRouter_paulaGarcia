5865ed415f6bbc0837715e60db9e85b1a7f32ac5
PaulaGDopico <paula.gdopico@gmail.com>
2023-11-14 20:03:17 +0100

    final clase

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/brazil">Brazil</RouterLink>
+        <RouterLink to="/panama">Panama</RouterLink>
+        <RouterLink to="/hawaii">Hawaii</RouterLink>
+        <RouterLink to="/jamaica">Jamaica</RouterLink>
       </nav>
-    </div>
   </header>
-
   <RouterView />
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeVue from '@/views/Home.vue'
+import HawaiiVue from '@/views/Hawaii.vue'
+import BrazilVue from '@/views/Brazil.vue'
+import JamaicaVue from '@/views/Jamaica.vue'
+import PanamaVue from '@/views/Panama.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +11,28 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeVue
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/1/brazil',
+      name: 'brazil',
+      component: BrazilVue
+    },
+    {
+      path: '/1/panama',
+      name: 'panama',
+      component: PanamaVue
+    },
+    {
+      path: '/1/hawaii',
+      name: 'hawaii',
+      component: HawaiiVue
+    },
+    {
+      path: '/1/jamaica',
+      name: 'jamaica',
+      component: JamaicaVue
+    },
   ]
 })
 
